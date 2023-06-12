8597476c49d540f59a63ee76c45c0c932268125d
AABITH-ROSHAN <130814060+AABITH-ROSHAN@users.noreply.github.com>
2023-06-12 14:38:29 +0530

    Add files via upload

---
+++ bus offer.py
@@ -0,0 +1,9 @@
+x=int(input("Enter birth year:"))
+y=int(input("Enter current year:"))
+z=y-x
+print("Age of the customer:, z")
+if(z>=60):
+    a=1020*(20/100)
+    print("The amni bus charge:",a)
+else:
+    print("The bus charge is 1020")
